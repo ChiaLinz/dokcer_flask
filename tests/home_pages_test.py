b3377a51bb1da6ea03f297e26bf5330de947fca1
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-02-24 14:36:17 -0500

    Update bootsratp home&about pages

---
+++ tests/home_pages_test.py
@@ -0,0 +1,15 @@
+"""This test the homepage"""
+
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/index")
+    assert response.status_code == 200
+    assert b"Chia-Lin's HomePage" in response.data
+
+def test_request_about(client):
+    """This makes the index page"""
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b"Chia-Lin's About Page" in response.data
+
