3c142aafe2db907c2244fe847a8e95dbce241b96
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-02-27 15:45:36 -0500

    Bug fix

--- tests/home_pages_test.py
+++ tests/home_pages_test.py
@@ -1,17 +1,15 @@
 """This test the homepage"""
 
-'''
+"""
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/index")
     assert response.status_code == 200
     assert b"Chia-Lin's HomePage" in response.data
-'''
+"""
 
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"Egestas" in response.data
-
-
